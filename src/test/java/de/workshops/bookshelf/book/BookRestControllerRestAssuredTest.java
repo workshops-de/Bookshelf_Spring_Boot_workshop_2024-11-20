5d4c1dd60da14a9e2b1e2abace1861c30ca6d4b9
Sandra Parsick <mail@sandra-parsick.de>
2024-11-22 09:56:02 +0100

    task: restassured

---
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerRestAssuredTest.java
@@ -0,0 +1,41 @@
+package de.workshops.bookshelf.book;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+//@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+class BookRestControllerRestAssuredTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+//    @Autowired
+//    private BookRestController bookRestController;
+
+    @Test
+    public void testWithRestAssuredMockMvc() {
+//        RestAssuredMockMvc.standaloneSetup(bookRestController);
+        RestAssuredMockMvc.mockMvc(mockMvc);
+        RestAssuredMockMvc.given().
+                    log().all().
+                when().
+                    get("/book").
+                then().
+                    log().all().
+                    statusCode(200).
+                    body("author[0]", equalTo("Erich Gamma"));
+    }
+
+
+}
