d30b9ef0732c0c891adfba0d76bb61634495e37b
Sandra Parsick <mail@sandra-parsick.de>
2024-11-22 09:57:57 +0100

    demo: mockito and junit5

---
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerPlainMockitoTest.java
@@ -0,0 +1,27 @@
+package de.workshops.bookshelf.book;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BookRestControllerPlainMockitoTest {
+
+    @Test
+    void getAllBook() {
+        BookService bookServiceMock = Mockito.mock(BookService.class);
+        BookRestController bookRestController = new BookRestController(bookServiceMock);
+        Mockito.when(bookServiceMock.allBooks()).thenReturn(List.of(new Book(), new Book(), new Book()));
+
+        final List<Book> books = bookRestController.getAllBooks();
+
+        assertThat(books).hasSize(3);
+    }
+
+}
