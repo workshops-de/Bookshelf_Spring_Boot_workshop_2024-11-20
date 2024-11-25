560d83054ab8b2b5ce3ef814f40b03de78fea69c
Sandra Parsick <mail@sandra-parsick.de>
2024-11-25 11:20:44 +0100

    task: Testconfiguration

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
@@ -1,11 +1,15 @@
 package de.workshops.bookshelf.book;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.workshops.bookshelf.ObjectMapperTestConfiguration;
+import groovy.transform.ImmutableBase;
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -18,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Import(ObjectMapperTestConfiguration.class)
 class BookRestControllerMockMvcTest {
 
     @Autowired
@@ -42,6 +47,7 @@ class BookRestControllerMockMvcTest {
 
 
         String jsonPayload = mvcResult.getResponse().getContentAsString();
+        System.out.println(jsonPayload);
         Book[] books = objectMapper.readValue(jsonPayload, Book[].class);
 
         assertThat(books).hasSize(3);
