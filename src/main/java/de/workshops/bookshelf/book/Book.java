ea40a1f8d417ca75a37e518662ccf6d97f2fa529
Sandra Parsick <mail@sandra-parsick.de>
2024-11-26 10:15:31 +0100

    task: init jpa entity

--- src/main/java/de/workshops/bookshelf/book/Book.java
+++ src/main/java/de/workshops/bookshelf/book/Book.java
@@ -1,12 +1,65 @@
 package de.workshops.bookshelf.book;
 
-import lombok.Data;
+import jakarta.persistence.*;
 
-@Data
+import java.util.Objects;
+
+@Entity
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
     private String title;
+    @Column
     private String description;
+    @Column
     private String author;
+    @Column
     private String isbn;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Book book)) return false;
+        return Objects.equals(id, book.id) && Objects.equals(title, book.title) && Objects.equals(description, book.description) && Objects.equals(author, book.author) && Objects.equals(isbn, book.isbn);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, author, isbn);
+    }
 }
