e6d184763684c185e2923df6a94d5f475e62956a
Sandra Parsick <mail@sandra-parsick.de>
2024-11-27 11:13:48 +0100

    task: credential

---
+++ src/main/java/de/workshops/bookshelf/SecurityConfig.java
@@ -0,0 +1,55 @@
+package de.workshops.bookshelf;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .build();
+    }
+
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//        var user = User.builder().username("user").password("password").roles("USER").build();
+//        var admin = User.builder().username("admin").password("admin").roles("USER", "ADMIN").build();
+//        return new InMemoryUserDetailsManager(user, admin);
+//    }
+
+    @Bean
+    public UserDetailsService userDetailsService(JdbcTemplate jdbcTemplate) {
+        return username -> {
+            String sql = "SELECT * FROM bookshelf_user WHERE username = ?";
+
+            return jdbcTemplate.queryForObject(sql, (rs, rowNum) -> new User(
+                    rs.getString("username"),
+                    rs.getString("password"),
+                    List.of(new SimpleGrantedAuthority(rs.getString("role")))
+            ), username);
+        };
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
