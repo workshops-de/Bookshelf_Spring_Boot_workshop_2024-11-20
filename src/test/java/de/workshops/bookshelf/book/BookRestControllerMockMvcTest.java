c46ded921fc79d9cd82b4eb622251daa3ec290fb
Sandra Parsick <mail@sandra-parsick.de>
2024-11-27 12:42:04 +0100

    task: mocktestuser

--- src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
+++ src/test/java/de/workshops/bookshelf/book/BookRestControllerMockMvcTest.java
@@ -10,6 +10,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -23,6 +24,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @Import(ObjectMapperTestConfiguration.class)
+@WithMockUser
 class BookRestControllerMockMvcTest {
 
     @Autowired
